d1e4ac780405354017a19d93efa8ad72d1a0756b
RJBHOMEW11\brink <brinkley908@gmail.com>
2023-04-08 18:45:44 +0100

    saved

--- TravelApi.Domain/Entity/Trip/Trip.cs
+++ TravelApi.Domain/Entity/Trip/Trip.cs
@@ -7,13 +7,13 @@ public class Trip
 
     private List<Segment> _segments = new();
     public IReadOnlyList<Segment> Segments => _segments.ToList();
-    public Guid Id { get; private set; }
-    public Guid CustomerId { get; private set; }
+    public TripId Id { get; private set; }
+    public CustomerId CustomerId { get; private set; }
     public string TripName { get; private set; } = string.Empty;
 
-    public static Trip Create(Guid customerId, string tripName) => new()
+    public static Trip Create(CustomerId customerId, string tripName) => new()
     {
-        Id = Guid.NewGuid(),
+        Id = new TripId(Guid.NewGuid()),
         CustomerId = customerId,
         TripName = tripName,
     };
