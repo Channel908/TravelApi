50bd5a59f8a8d64f5b91c6a7a1071d8cb1574d52
RJBHOMEW11\brink <brinkley908@gmail.com>
2023-04-13 17:03:37 +0100

    Add Generic and CustomerRepository

---
+++ TravelApi.Application/Interface/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using TravelApi.Domain.Entity;
+
+namespace TravelApi.Application.Interface;
+
+public interface ICustomerRepository : IRepository<Customer>
+{
+    Task<List<Customer>> GetByLastName(string lastName);
+}
---
+++ TravelApi.Persistence/Repository/CustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TravelApi.Application.Interface;
+using TravelApi.Domain.Entity;
+
+namespace TravelApi.Persistence.Repository;
+
+public class CustomerRepository : RepositoryBase<Customer>, ICustomerRepository
+{
+    private readonly DbSet<Customer> _set;
+    public CustomerRepository(ApplicationDbContext context) : base(context)
+    {
+        _set = context.Set<Customer>();
+    }
+
+    public async Task<List<Customer>> GetByLastName(string lastName)
+        => await _set
+            .Where(x => x.LastName == lastName)
+            .ToListAsync();
+}
